b34efcedc710d9bec140af7d43e49be13012a2f1
d-zucho <danny.nico.dev@outlook.com>
2025-11-01 03:19:37 -0700

    add login UI

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,48 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { GithubIcon } from 'lucide-react'
+
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login Page</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle className='text-xl'>Welcome back!</CardTitle>
+          <CardDescription>
+            Login with your Github Email Account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='flex flex-col gap-4'>
+          <Button className='w-full' variant={'outline'}>
+            <GithubIcon className='size-4' />
+            Sign in with GitHub
+          </Button>
+
+          <div className='relative text-sm text-center after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
+            <span className='relative z-10 bg-card px-2 text-muted-foreground'>
+              Or continue with
+            </span>
+          </div>
+
+          {/* email input */}
+          <div className='grid gap-3'>
+            <div className='grid gap-2'>
+              <Label htmlFor='email'>Email</Label>
+              <Input type='email' placeholder='johndoe@example.com' />
+            </div>
+
+            <Button>Continue with Email</Button>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   )
 }
